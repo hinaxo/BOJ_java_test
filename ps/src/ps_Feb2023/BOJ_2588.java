b7b0c670c716f4649aa250cb91621be2596c3e48
heojaeeun <jxford123@gmail.com>
2023-02-07 21:59:17 +0900

    List, ArrayList, Array, Set

--- ps/src/ps_Feb2023/BOJ_2588.java
+++ ps/src/ps_Feb2023/BOJ_2588.java
@@ -1,6 +1,29 @@
 package ps_Feb2023;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_2588 {
-	public static void main(String[] args) {
-		
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer tokens;
+
+	static int firstNum;
+	static String secondNum;
+	public static void main(String[] args) throws IOException {
+		firstNum = Integer.parseInt(br.readLine());
+		secondNum = br.readLine();
+		for(int i = secondNum.length(); i > 0; i--) {
+			secondNum.substring(i-1, i);
+			System.out.println(firstNum * Integer.parseInt(secondNum.substring(i-1, i)));
+			//(2, 3) 잘라서 'secondNum의 일의자리' * firstNum 
+			//(1, 2) 잘라서 'secondNum의 십의자리' * firstNum 
+			//(0, 1) 잘라서 'secondNum의 백의자리' * firstNum 
+		}
+		System.out.println(firstNum * Integer.parseInt(secondNum));
+	//전체 곱셈 출력 
 	}
 }
---
+++ ps/src/ps_Feb2023/BOJ_3052.java
@@ -0,0 +1,44 @@
+package ps_Feb2023;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ_3052 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer tokens;
+	
+	public static void main(String[] args) throws IOException {
+		String[] numList = {};
+		for(int i=0; i<10; i++) {
+			String num = br.readLine();
+			int left = Integer.parseInt(num) % 42;
+			
+			numList = Add(numList, Integer.toString(left));
+		}		
+		List<String> list = Arrays.asList(numList);
+		Set<String> set = new HashSet<String>(list);
+		List<String> newList = new ArrayList<String>(set);
+		String[] resultArr = newList.toArray(new String[0]);
+
+		System.out.println(resultArr.length);
+	}
+
+	private static String[] Add(String[] numList, String num) {
+		// TODO Auto-generated method stub
+		String[] newArray = new String[numList.length + 1];
+		for(int index=0; index<numList.length; index++) {
+			newArray[index] = numList[index];
+		}
+		newArray[numList.length] = num;
+		return newArray;
+	}
+}
